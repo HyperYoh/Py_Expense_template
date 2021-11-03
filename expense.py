2636b196df215dcd65813f783975376900ae6d15
HyperYoh <hyper1yoh@gmail.com>
2021-11-03 21:25:13 +0100

    complete spender choosed among existing user when new expense

--- expense.py
+++ expense.py
@@ -1,5 +1,7 @@
 from PyInquirer import prompt
 import csv
+import copy
+
 
 expense_questions = [
     {
@@ -12,18 +14,26 @@ expense_questions = [
         "name":"label",
         "message":"New Expense - Label: ",
     },
-    {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
-    },
-
 ]
 
+choose_user={
+        "type":"list",
+        "name":"choose_user",
+        "message":"New Expense - Spender: ",
+        "choices": []
+}
+
+def fun_choose_user():
+    dico = copy.deepcopy(choose_user)
+    with open("users.csv", newline="") as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=["Name"])
+        dico["choices"] += [row["Name"] for row in reader]
+    return dico
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    user=prompt(fun_choose_user())["choose_user"]
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     with  open("expense_report.csv", newline="") as csvfile:
         reader=csv.DictReader(csvfile)
@@ -35,6 +45,6 @@ def new_expense(*args):
                 writer.writeheader()
     with open("expense_report.csv", "a", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({"Amount": infos['amount'], "Label": infos['label'], "Spender": infos['spender']})
+        writer.writerow({"Amount": infos['amount'], "Label": infos['label'], "Spender": user})
     print("Expense Added !")
     return True
