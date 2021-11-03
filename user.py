256c936c376635d8130b0b43d6bff374c7c5f6b6
HyperYoh <hyper1yoh@gmail.com>
2021-11-03 21:00:03 +0100

    complete create persistency user

--- user.py
+++ user.py
@@ -1,8 +1,26 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    }
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    fieldnames=["Name"]
+    with open("users.csv", newline="") as csvfile:
+        reader=csv.DictReader(csvfile, fieldnames=fieldnames)
+        for row in reader:
+            if row["Name"] == infos["name"]:
+                print(f"Error: User \"{infos['name']}\" already in database " )
+                return False
+    with open("users.csv", "a", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({"Name": infos["name"]})
+        print("User Added !")
+    return True
